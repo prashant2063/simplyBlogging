98951f918bb0a76b3fa961d962e7912b0294f497
Prashant GUpta <prashant2063@gmail.com>
2020-08-23 23:22:38 +0530

    added contact and about page

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ var articleDesc = document.getElementsByClassName("article-desc");
 for(var i = 0, length = articleDesc.length; i < length; i++) {
     var myArticle = articleDesc[i].innerHTML;     
     articleDesc[i].innerHTML = myArticle.substring(0,200)+"...";
- }
-// console.log(myArticle);
-// articleDesc.innerHTML = myArticle.substring(0,10);
+}
